38479135da34b31e0340decbc72996c0917958c0
robinson732 <kimanirobinson336@gmail.com>
2025-08-19 14:02:32 +0300

    final

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,24 @@
+# Superclass
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
 
+    def raise_hand(self):
+        print("Pick me!")
+
+# Subclass
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        # Call the parent class method
+        super().hello()
+        # Add the chatty extra phrase
+        print("How are you doing today? I'm okay, but I'm kind of tired. "
+              "Did you watch The Walking Dead last night? You didn't?! "
+              "Oh man, it was so crazy! What, you don't want any spoilers? "
+              "Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        # Call the parent class method ten times
+        for _ in range(10):
+            super().raise_hand()
+
